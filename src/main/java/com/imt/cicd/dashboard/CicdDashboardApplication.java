c8e35aa147b967bd0dd7d4b7a765e7f1743dd292
Théo Lebiez <theo.lebiez@etu.imt-nord-europe.fr>
2026-01-05 09:40:41 +0100

    init project

---
+++ src/main/java/com/imt/cicd/dashboard/CicdDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.imt.cicd.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imt/cicd/dashboard/CicdDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imt.cicd.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
